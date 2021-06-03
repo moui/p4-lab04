7eef965c329461b1baf2acc71c7a02e1213094e7
moui <mouibarrera8@gmail.com>
2021-06-03 17:23:50 -0300

    agregado fabrica.h

---
+++ lib/Fabrica.h
@@ -0,0 +1,22 @@
+#ifndef _FABRICA
+#define _FABRICA
+
+#include "IUsuario.h"
+#include "CtrlUsuario.h"
+
+using namespace std;
+
+class Fabrica
+{
+    private:
+	    static Fabrica * instancia;
+	    Fabrica();
+
+    public:
+        ~Fabrica();
+	    static Fabrica * getInstancia();
+	    
+        IUsuario * getIUsuario();
+};
+
+#endif
