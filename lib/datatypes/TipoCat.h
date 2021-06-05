7b4b415d90b573fc37516cc0d8afe8c89e01ece0
moui <mouibarrera8@gmail.com>
2021-06-05 17:51:54 -0300

    TipoCat.cpp

--- lib/datatypes/TipoCat.h
+++ lib/datatypes/TipoCat.h
@@ -11,37 +11,4 @@ enum class TipoCat
     Otro = 2,
     };
 
-
-    inline std::istream& operator>> ( std::istream& in, TipoCat& t )
-    {
-    int valor;
-
-    if ( in  >> valor ) {
-        switch ( valor ) {
-        case 0: case 1 : case 2:
-        t = TipoCat(valor); break;
-        default:
-        throw std::out_of_range ( "Por favor, escriba uno de los tipos detallados.\n" );
-        }
-    }
-
-    return in;
-    }
-
-
-    inline std::ostream& operator<<(std::ostream& out, const TipoCat& t){
-    switch (t) {
-    case TipoCat::Plataforma:
-        out << "Plataforma";
-        break;
-    case TipoCat::Genero:
-        out << "Genero";
-        break;
-    case TipoCat::Otro:
-        out << "Otro";
-        break;
-    }
-    return out;
-}
-
 #endif
