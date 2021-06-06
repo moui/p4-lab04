19b179c8bf5914824388fd4a44fc60d53b5f7398
Maxuos4 <80793493+Maxuos4@users.noreply.github.com>
2021-06-06 00:51:58 -0300

    Update PartidaMultijugador.h

--- lib/PartidaMultijugador.h
+++ lib/PartidaMultijugador.h
@@ -10,6 +10,8 @@ class PartidaMultijugador : public Partida
     bool transmitidaEnVivo;
     InfoPartiddaJugador **unen;
     int unidos;
+    Comentario* chat;
+    
   public:
 
     PartidaMultijugador(DtFechaHora , float, bool, Jugador *, Videojuego *, Jugador **, std::string*, int);
@@ -21,12 +23,16 @@ class PartidaMultijugador : public Partida
     bool getEnVivo();
     int getUnidos();
     std::string* getNicknames();
+    DtFechaHora* getHoraAbandono();
+    Comentario getComentario();
 
 
 //setters
     void setEnVivo(bool);
     void setUnidos(int);
     void setNicknames(std::string*);
+    void setHoraAbandono();
+    void comentar();
 
 };
 #endif
