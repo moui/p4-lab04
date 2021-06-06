759e7f05aeacd0231e92abfb4f1be3014246e950
NahuelPereyraF <80930589+NahuelPereyraF@users.noreply.github.com>
2021-06-06 19:18:26 -0300

    Esqueleto CtrlPartida

---
+++ src/CtrlPartida.cpp
@@ -0,0 +1,52 @@
+#include "../lib/CtrlPartida.h"
+
+using namespace std;
+
+CtrlPartida * CtrlPartida::instancia = NULL;
+    
+CtrlPartida::~CtrlPartida(){
+
+}
+    
+CtrlPartida * CtrlPartida::getCtrPartida(){
+    if (instancia == NULL){
+        instancia = new CtrlCtrlPartida();
+    }
+    return instancia;
+}
+
+void crearPartida(string nombreVJ){
+
+}
+
+void partidaAContinuar(float id){
+
+}
+void listaJugUnidos(set<string> nicknames){
+
+}
+set<string> listaJugSuscriptos(){
+
+}
+void confirmarIniciarPartida(){
+
+}
+set<DtPartidaIndividual> listaPartidasIndTer(){
+
+}
+void enVivo(bool enVivo){
+
+}
+void cancelarIniciarPartida(){
+
+}
+set<DtPartida> listaPartidasIniciadas(string nickname){
+
+}
+void finalizarPartida(float id){
+
+}
+void cancelarFinalizarPartida(){
+
+}
+
