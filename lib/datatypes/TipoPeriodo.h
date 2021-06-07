896831fd7e73306c673b88f7ccf8f5b66d2bb076
nodescanses <ivngtz11@gmail.com>
2021-06-07 09:03:11 -0300

    tipoPeriodo y DescripcionSuscripcion

---
+++ lib/datatypes/TipoPeriodo.h
@@ -0,0 +1,15 @@
+#ifndef DT_TIPOPERIODO
+#define DT_TIPOPERIODO
+
+#include <iostream>
+
+enum class TipoPeriodo
+    {
+
+    mensual = 0,
+    trimestral = 1,
+    anual = 2,
+    vitalicia= 3,
+    };
+
+#endif
