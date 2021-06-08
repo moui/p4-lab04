1fa035a1aa495117831c3dea4a94014a2d00642d
nodescanses <ivngtz11@gmail.com>
2021-06-08 18:09:05 -0300

    sobrecarga

--- src/datatypes/TipoPuntaje.cpp
+++ src/datatypes/TipoPuntaje.cpp
@@ -1,1 +1,38 @@
 #include "lib/datatypes/TipoPuntaje.h"
+#include <iostream>
+
+inline std::istream& operator>> ( std::istream& in, TipoPeriodo& t ){
+    int valor;
+
+    if ( in  >> valor ) {
+        switch ( valor ) {
+            case 1: case 2 : case 3: case 4: case 5:
+                t = TipoPuntaje(valor); break;
+            default:
+                throw std::out_of_range ( "Por favor, escriba uno de los tipos detallados.\n" );
+        }
+    }
+    return in;
+}
+
+
+inline std::ostream& operator<<(std::ostream& out, const TipoPuntaje& t){
+    switch (t) {
+        case TipoPuntaje::uno:
+            out << "Uno";
+            break;
+        case TipoPuntaje::dos:
+            out << "Dos";
+            break;
+        case TipoPuntaje::tres:
+            out << "Tres";
+            break;
+        case TipoPuntaje::cuatro:
+            out << "Cuatro";
+            break;
+        case TipoPuntaje::cinco:
+            out << "Cinco";
+            break;
+    }
+    return out;
+}
