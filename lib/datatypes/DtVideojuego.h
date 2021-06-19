a429e2298bea5af6057a8908885c3cab6369f2aa
jprubiocolucci <jprubiocolucci@gmail.com>
2021-06-19 17:20:08 -0300

    dtvj

--- lib/datatypes/DtVideojuego.h
+++ lib/datatypes/DtVideojuego.h
@@ -1,6 +1,8 @@
 #ifndef DT_VIDEOJUEGO
 #define DT_VIDEOJUEGO
 
+#include "DtCategoria.h"
+
 #include <string>
 #include <ostream>
 #include <set>
@@ -14,12 +16,12 @@ class DtVideojuego
     private:
         string nombre, descripcion;
         float costo1, costo3, costo12, costoV; 
-        float* totalHorasDeJuego;
+        set<DtCategoria*> categorias;
 
     public:
         // CTOR y DTOR
         DtVideojuego(string nombre, string descripcion, float costo1, float costo3, float costo12, 
-            float costoV, float* totalHorasDeJuego);
+            float costoV, set<DtCategoria*> categorias);
 
         ~DtVideojuego();
 
@@ -30,7 +32,7 @@ class DtVideojuego
         float getCosto3();
         float getCosto12();
         float getCostoV();
-        float* getTotalHorasDeJuego();
+        set<DtCategoria*> getCategorias();
 
         // Sobrecarga
         friend ostream& operator<<(ostream& out, const DtVideojuego& dt);
