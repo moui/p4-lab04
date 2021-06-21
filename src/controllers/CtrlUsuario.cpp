6c34c6b90642cedd8b5399ff1ab6e7f6b5f08a8b
jprubiocolucci <jprubiocolucci@gmail.com>
2021-06-20 23:12:06 -0300

    aaaaaaaaaaaaaaaaaaaaa

--- src/controllers/CtrlUsuario.cpp
+++ src/controllers/CtrlUsuario.cpp
@@ -167,6 +167,9 @@ DtUsuario* CtrlUsuario::iniciarSesion(string mail, string contrasena){
 
     TipoEstado CtrlUsuario::getEstadoSuscripcion(string nombreVideojuego)
     {
+        if (CtrlVideojuego::getCtrlVideojuego()->getVJ(nombreVideojuego) == NULL)
+            throw invalid_argument("Videojuego " + nombreVideojuego + " no existe en el catalogo.");
+
         Usuario* user= CtrlUsuario::getSesionActiva();
         if (dynamic_cast<Jugador*>(user) == NULL)
             throw invalid_argument("Usuario loggeado debe ser jugador");
